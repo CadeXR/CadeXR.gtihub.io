474e1f30868814be76186ef0e69bb556650c4bc2
CadeXR <54557425+CadeXR@users.noreply.github.com>
2025-04-09 15:56:53 -0400

    404 fix in next.

--- next.config.js
+++ next.config.js
@@ -1,18 +1,24 @@
 /** @type {import('next').NextConfig} */
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = ''
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
 const nextConfig = {
   output: 'export',
-  basePath: '/CadeXR.github.io',
-  assetPrefix: '/CadeXR.github.io/',
+  basePath: basePath,
+  assetPrefix: assetPrefix,
   images: {
     unoptimized: true,
   },
-  // Ensure trailing slashes are handled correctly
   trailingSlash: true,
 }
 
 module.exports = nextConfig
-
-
-
-
 
