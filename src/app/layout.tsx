3e58b7eaa9229e7f9e5fc3001d620feeaa3d7b77
CadeXR <54557425+CadeXR@users.noreply.github.com>
2025-04-11 13:25:03 -0400

    added .ico

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,23 @@ import { conthrax } from './fonts'
 export const metadata: Metadata = {
   title: 'Cade Gilbert',
   description: 'XR Design, XR Software Dev, Game Design, and AI',
+  icons: {
+    icon: '/media/VR_Knight_Transparent.ico',
+    shortcut: '/media/VR_Knight_Transparent.ico',
+  },
+  openGraph: {
+    title: 'Cade Gilbert',
+    description: 'XR Design, XR Software Dev, Game Design, and AI',
+    url: 'https://www.cadedev.com',
+    siteName: 'Cade Gilbert',
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Cade Gilbert',
+    description: 'XR Design, XR Software Dev, Game Design, and AI',
+  }
 }
 
 export default function RootLayout({
@@ -18,6 +35,7 @@ export default function RootLayout({
     </html>
   )
 }
+
 
 
 
